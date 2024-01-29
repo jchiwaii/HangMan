afc1d95d7ef882d58cb925c638cc5aec80eacaff
Chiwai <157381163+jchiwaii@users.noreply.github.com>
2024-01-29 06:34:52 +0300

    Contains words used

---
+++ word_list.py
@@ -0,0 +1,7 @@
+word_list = {
+    'Animals': ['abyss', 'beekeeper', 'buffalo', 'buzzard', 'cobra', 'foxglove', 'jaguar', 'jaywalk', 'jockey', 'kangaroo', 'lynx', 'puppy', 'quail', 'vixen', 'wombat', 'yak', 'zebra'],
+    'Countries': ['albania', 'belgium', 'cambodia', 'denmark', 'egypt', 'finland', 'ghana', 'hungary', 'india', 'jamaica', 'kenya', 'luxembourg', 'mexico', 'netherlands', 'oman', 'poland', 'qatar', 'russia', 'spain', 'turkey', 'uganda', 'vietnam', 'yemen', 'zimbabwe'],
+    'Sports': ['badminton', 'cricket', 'dodgeball', 'equestrian', 'fencing', 'golf', 'hockey', 'judo', 'kayaking', 'lacrosse', 'netball', 'olympics', 'polo', 'quidditch', 'rugby', 'soccer', 'tennis', 'volleyball', 'wrestling', 'yoga', 'zumba'],
+    'Science': ['atom', 'biosphere', 'cell', 'dna', 'electron', 'fusion', 'genome', 'hydrogen', 'isotope', 'joule', 'kelvin', 'laser', 'molecule', 'neutron', 'oxygen', 'photon', 'quantum', 'radiation', 'spectrum', 'thermodynamics', 'universe', 'voltage', 'wavelength', 'xenon', 'yield', 'zinc'],
+    'Cities': ['amsterdam', 'berlin', 'chicago', 'dublin', 'edinburgh', 'florence', 'geneva', 'helsinki', 'istanbul', 'jakarta', 'kyoto', 'lisbon', 'madrid', 'naples', 'oslo', 'paris', 'quito', 'rome', 'seoul', 'tokyo', 'utrecht', 'vienna', 'warsaw', 'xian', 'york', 'zagreb']
+}
